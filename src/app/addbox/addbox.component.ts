5b949bea244f56cfbac8c0e24dd0ab6c11bedff9
The1Re <kanesh132@gmail.com>
2023-03-06 23:27:40 +0700

    Update Add book

--- src/app/addbox/addbox.component.ts
+++ src/app/addbox/addbox.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { databook } from '../searchbox/data-book'
 
 @Component({
   selector: 'app-addbox',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['../searchbox/searchbox.component.css']
 })
 export class AddboxComponent {
+  booklist = databook;
 
+  addData(name:string,category:string,credit:string,price:string,borrower:string,date:string){
+    this.booklist.push({
+      name:name,
+      category:category,
+      credit:credit,
+      price:price,
+      borrower:borrower,
+      date:date
+    })
+    Swal.fire("Thank You!","You have added","success")
+  }
 }
